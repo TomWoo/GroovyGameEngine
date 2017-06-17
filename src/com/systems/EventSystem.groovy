d6e723382057e16d4e98fb230336d348b76bb841
TomWoo <tomw.82008@gmail.com>
2017-06-16 22:29:06 -0700

    added method tracing, about to refactor

--- src/com/systems/EventSystem.groovy
+++ src/com/systems/EventSystem.groovy
@@ -21,4 +21,35 @@ class EventSystem extends System {
             }
         }
     }
+
+    def trace(Object obj) {
+        MetaClass c = obj.metaClass
+        c.invokeMethod = { String name, Object... args ->
+            println("$obj.$name($args) called (tracing object).")
+            def result = c.getMetaMethod(name, args).invoke(delegate, args)
+            return result
+            //delegate.invokeMethod(name, args) // TODO: delegate."$name"(args)
+        }
+    }
+
+    def traceMethod(Object obj, String methodName, Object[] methodArgs) { // TODO: condition(s)
+        MetaClass c = obj.metaClass
+        /*
+        c.invokeMethod = { String name, Object... args ->
+            if(name.equals(methodName)) {
+                println("$obj.$name($args) called (tracing method).")
+            }
+            //def result = c.getMetaMethod(name, args).invoke(delegate, args)
+            //return result
+        }
+        */
+        c."$methodName" = {
+            println("$obj.$methodName($methodArgs) called (tracing method).")
+        }
+        //c.invokeMethod(methodName, methodArgs)
+    }
+
+    def test() {
+        println("string")
+    }
 }
